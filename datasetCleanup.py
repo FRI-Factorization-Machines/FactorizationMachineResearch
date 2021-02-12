ab69b4a050c496be064898475c2c5e1f4b020114
Victor Favela <favelavicdan478@hotmail.com>
2021-02-11 18:38:54 -0600

    Create datasetCleanup.py

---
+++ datasetCleanup.py
@@ -0,0 +1,10 @@
+## This file is just a sample file for reading in datasets, cleaning them up, and combining into one larger dataset
+
+import numpy as np
+import pandas as pd
+
+## Steps:
+## Read in both Dataset files and local files that are too large to be on github 
+## Clean datasets  
+## Combine to one dataset 
+## Export that file localy  
