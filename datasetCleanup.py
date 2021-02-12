c23721e6d2146eb422c45ae1394a184dfc47fd2c
brandonvietlam <56048840+brandonvietlam@users.noreply.github.com>
2021-02-12 01:15:15 -0600

    Add ability to combine zip and csv datasets

--- datasetCleanup.py
+++ datasetCleanup.py
@@ -2,9 +2,25 @@
 
 import numpy as np
 import pandas as pd
+import zipfile
 
 ## Steps:
 ## Read in both Dataset files and local files that are too large to be on github 
+data = pd.read_csv(r'Datasets/WellTransferData.csv', dtype='unicode')
+
+zf = zipfile.ZipFile(r'Datasets/wellDOS.zip') 
+local = pd.read_csv(zf.open('wellspublic.csv'), dtype='unicode')
+
+api_col = data.columns[0]
+local_api_col = local.columns[0]
+
+data[api_col] = data[api_col].str.replace(r'[^0-9]+', '')
+
 ## Clean datasets  
+
 ## Combine to one dataset 
-## Export that file localy  
+# how='outer' obtains union of APIs, 'inner' obtains intersection of APIs
+merged = local.merge(data, how='outer', left_on=local_api_col, right_on=api_col)
+
+## Export that file localy
+merged.to_csv(r'Datasets/mergedDataset.tsv', sep='\t')
